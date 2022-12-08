fad8be5fd4c8cbdee7c564273062764c0cbb8863
Ahsanulnas Ahsanulnas <ahsanulnasahsanulnas@10.0.0.9>
2022-12-08 20:55:47 +0100

    br server

--- app/main.py
+++ app/main.py
@@ -16,25 +16,21 @@ API_URL = {
 @app.get("/api/smart/{timeoutms}")
 async def smart_api(timeoutms: int):
     res = []
-    def service(timeout_s, attempt):
+    timeout_s = float((timeoutms if timeoutms > 0 else 300)/1000)
+    for _ in range(3):
         try:
-            trial = attempt - 1
+            response = requests.get(API_URL['EXPO'], timeout=timeout_s)
+            if response.status_code == 200: 
+                res.append(response.json()['time'])
+                break
+            elif response.status_code == 429:
+                raise HTTPException(status_code=429, detail="EXPO API has too many request!")
+            else:
+                raise HTTPException(status_code=500, detail="Internal Server Error")
 
-            if trial >= 0:
-                response = requests.get(API_URL['EXPO'], timeout=timeout_s)
-                if response.status_code == 200: 
-                    return res.append(response.json()['time'])
-                elif response.status_code == 429:
-                    raise HTTPException(status_code=429, detail="EXPO API has too many request!")
-                else:
-                    raise HTTPException(status_code=404, detail="Item are not found.")
-            return None    
-            
         except Timeout as ex:
-            logger.warning(f"Exception raised: {ex}. Attempt %d" % trial)
-            service(timeout_s, trial)                
+            raise HTTPException(status_code=408, detail="Timeout has been raised.")
 
-    service(float((timeoutms if timeoutms > 0 else 300)/1000), 3)
-    if len(res) == 0:
-        raise HTTPException(status_code=408, detail="Attempt limit exceeded.")
+    if len(res) ==  0:
+        raise HTTPException(status_code=404, detail="Item are not found.")
     return {"Time": res[0]}
--- app/test_main.py
+++ app/test_main.py
@@ -9,16 +9,19 @@ client = TestClient(app)
 def test_smart_api():
     response = client.get("/api/smart/300")
     if response.status_code == 200:
-        assert type(response.json()['Time']) == type(random.randint(1,300))
+        assert isinstance(response.json()['Time'], int)
 
     if response.status_code == 429:
         assert response.json() == {"detail": "EXPO API has too many request!"}
 
     if response.status_code == 408:
-        assert response.json() == {"detail": "Attempt limit exceeded."}
+        assert response.json() == {"detail": "Timeout has been raised."}
 
     if response.status_code == 404:
         assert response.json() == {"detail": "Item are not found."}
+    
+    if response.status_code == 500:
+        assert response.json() == {"detail": "Internal Server Error"}
 
 
 def test_smart_api_check_body():
